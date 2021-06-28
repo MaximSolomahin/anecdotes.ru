650e1d16a613585710571bae535bb269432407f0
Maxim Solomakhin <nogiubogi0@gmail.com>
2021-06-28 20:18:06 +0300

    Update header.php

--- blocks/header.php
+++ blocks/header.php
@@ -2,7 +2,7 @@
 <html lang="ru">
 	<head>
 		<meta charset="utf-8">  
-		<title>Гостевая книга</title>
+		<title><?=$title?></title>
 		<link rel="stylesheet" href="/css/bootstrap/css/bootstrap.css">
 		<link rel="stylesheet" href="/css/styles.css">
 	</head>
